f644c51a0637b584b0a6c0d8bcff4e1e5cacf1fe
JingeLi <jinge.li@thoughtworks.com>
2020-06-14 16:12:58 +0800

    finish Practice1 and 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = input =>{
+    let result = [];
+    for(let i = 0; i < input.data.length; ++i){
+        let obj = {};
+        for(let j = 0; j < input.column.length; ++j){
+            let name = input.column[j].name;
+            obj[name] = input.data[i][j];
+        }
+        result.push(obj);
+    }
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+    //逆序排序，前面插入之后会影响后面的index插入位置
+    sections.sort((a, b) => b.index - a.index);
+    for(let obj of sections){
+        let index = obj.index;
+        items.splice(index, 0, obj.content);
+    }
+    return items;
+}
 export { inject };
